84ba8ead4dd5044c91b4777d9acfde300cea055e
Srijukta <87535476+srijukta06@users.noreply.github.com>
2024-06-30 02:48:31 +0530

    Create Prime_factors.py

---
+++ Prime_factors.py
@@ -0,0 +1,12 @@
+# Write a Python function to find all prime factors.
+
+def get_prime_factors(number):
+  factors = []
+  divisor = 2
+  while divisor <= number:
+    if number % divisor == 0:
+      factors.append(divisor)
+      number = number // divisor
+    else:
+      divisor += 1
+  return factors
