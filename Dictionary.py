a9851d4509aba043312f4f6559fbd3fa5141d46f
Srijukta <87535476+srijukta06@users.noreply.github.com>
2024-06-30 23:03:01 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+#Write a Python function to save a dictionary to file.
+
+import pickle
+def save_dict(dict_to_save, file_path): 
+  with open(file_path, 'wb') as file: 
+    pickle.dump(dict_to_save, file)
+
+def load_dict(file_path): 
+  with open(file_path, 'rb') as file: 
+    return pickle.load(file)
