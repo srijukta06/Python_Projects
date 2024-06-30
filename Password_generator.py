73e7f89ffe15f4427ae042c63dff6ac36afd7ad8
Srijukta <87535476+srijukta06@users.noreply.github.com>
2024-07-01 00:07:33 +0530

    Create Password_generator.py

---
+++ Password_generator.py
@@ -0,0 +1,11 @@
+#Write a Python function to generate passphrases
+
+import secrets
+
+def generate_passphrase(num_words, wordlist_path='diceware.wordlist.asc'):
+  with open(wordlist_path, 'r', encoding='utf-8') as file:
+    lines = file.readlines()[2:7778]
+    word_list = [line.split()[1] for line in lines]
+
+  words = [secrets.choice(word_list) for i in range(num_words)]
+  return ' '.join(words)
